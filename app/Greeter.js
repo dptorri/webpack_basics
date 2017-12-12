9c593cb9b25dbf9f4d9dce300daa0d66fd668b60
Daniel Palacios <dptorri@gmail.com>
2017-12-12 11:54:06 +0100

    react refactoring
    Step11 React install and refactoring

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,8 +1,23 @@
-var config = require('./config.json');
+import React, {Component} from 'react'
+import config from './config.json'
+
+class Greeter extends Component{
+    render(){
+        return (
+            <div>
+                {config.greetText}
+            </div>
+        );
+    }
+}
+
+export default Greeter
+/* Step10: refactor this code below
+ var config = require('./config.json');
 
 module.exports = function(){
     var greet = document.createElement('div');
     greet.textContent = config.greetText;
     //greet.textContent = "Hi there, greetings from me ;) ";
     return greet;
-};
+};*/
--- app/main.js
+++ app/main.js
@@ -1,2 +1,13 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Greeter from './Greeter';
+
+render (<Greeter />, document.getElementById('root'));
+
+
+
+
+/* Refactoring code to ES6 modules
 var greeter = require('./Greeter.js');
 document.getElementById('root').appendChild(greeter());
+*/
