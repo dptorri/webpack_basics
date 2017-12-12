e7aa904acde6b11c031c3dc2fb22ebcb71a83898
Daniel Palacios <dptorri@gmail.com>
2017-12-12 14:55:47 +0100

    build for production
    Is a good practice to split the webpack.config.js file so we create a production file were we stripped devtool, devServer and HotModuleReplacement

---
+++ webpack.production.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'eval-source-map',
+    entry: __dirname + "/app/main.js",
+    output: {
+        path: __dirname + "/build",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {test: /\.json$/,loader: "json-loader"},
+            {test: /\.js$/,exclude: /node_modules/,loader: 'babel-loader'},
+            {test: /\.css$/,loader: 'style-loader!css-loader?modules'},
+            {test: /\.css$/,loader: 'postcss-loader',
+                options: {
+                    ident: 'postcss',
+                    plugins: (loader) => [
+                    require('autoprefixer')()
+                    ]
+                }
+            }         
+        ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+          template: __dirname + '/app/index.tmpl.html'
+      })
+    ]
+}
